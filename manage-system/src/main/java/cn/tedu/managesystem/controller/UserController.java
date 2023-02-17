43cce3861437c39f3169feb0e3cf139cad0aab91
itlongchen <1965399133@qq.com>
2023-02-17 19:03:32 +0800

    添加sql语句文件

--- manage-system/src/main/java/cn/tedu/managesystem/controller/UserController.java
+++ manage-system/src/main/java/cn/tedu/managesystem/controller/UserController.java
@@ -1,11 +1,7 @@
 package cn.tedu.managesystem.controller;
 
-
-import cn.tedu.managesystem.pojo.dto.UserLoginDto;
 import cn.tedu.managesystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
